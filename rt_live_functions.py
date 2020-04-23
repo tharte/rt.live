f27771dfc3d81e946be65f14a01d44d6cf718b41
Thomas P. Harte <tharte@cantab.net>
2020-04-23 19:09:25 -0400

    add function highest_density_interval to rt_live_functions.py

--- rt_live_functions.py
+++ rt_live_functions.py
@@ -12,3 +12,30 @@ from scipy import stats as sps
 from scipy.interpolate import interp1d
 
 FILTERED_REGION_CODES = ['AS', 'GU', 'PR', 'VI', 'MP']
+
+def highest_density_interval(pmf, p=.9, debug=False):
+    # If we pass a DataFrame, just call this recursively on the columns
+    if (isinstance(pmf, pd.DataFrame)):
+        return pd.DataFrame(
+            [highest_density_interval(pmf[col], p=p) for col in pmf],
+            index=pmf.columns
+        )
+
+    cumsum = np.cumsum(pmf.values)
+
+    # N x N matrix of total probability mass for each low, high
+    total_p = cumsum - cumsum[:, None]
+
+    # Return all indices with total_p > p
+    lows, highs = (total_p > p).nonzero()
+
+    # Find the smallest range (highest density)
+    best = (highs - lows).argmin()
+    low = pmf.index[lows[best]]
+    high = pmf.index[highs[best]]
+
+    return pd.Series(
+        [low, high],
+        index=[f'Low_{p*100:.0f}',
+        f'High_{p*100:.0f}']
+    )
