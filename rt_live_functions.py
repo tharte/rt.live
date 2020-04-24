b268605ec7edb4d52a1bfefddbe1cec0eaaab7e4
Thomas P. Harte <tharte@cantab.net>
2020-04-23 22:11:38 -0400

    get_posteriors to rt_live_functions

--- rt_live_functions.py
+++ rt_live_functions.py
@@ -56,3 +56,63 @@ def prepare_cases(cases, cutoff=25):
     original = new_cases.loc[smoothed.index]
 
     return original, smoothed
+
+def get_posteriors(sr, sigma=0.15, gamma=1/7, r_t_range=None, R_T_MAX=12):
+    if r_t_range is None:
+        r_t_range = np.linspace(0, R_T_MAX, R_T_MAX*100+1)
+
+    # (1) Calculate Lambda
+    lam = sr[:-1].values * np.exp(gamma * (r_t_range[:, None] - 1))
+
+    # (2) Calculate each day's likelihood
+    likelihoods = pd.DataFrame(
+        data = sps.poisson.pmf(sr[1:].values, lam),
+        index = r_t_range,
+        columns = sr.index[1:]
+    )
+
+    # (3) Create the Gaussian Matrix
+    process_matrix = sps.norm(
+        loc=r_t_range,
+        scale=sigma
+   ).pdf(r_t_range[:, None]) 
+
+    # (3a) Normalize all rows to sum to 1
+    process_matrix /= process_matrix.sum(axis=0)
+
+    # (4) Calculate the initial prior
+    #prior0 = sps.gamma(a=4).pdf(r_t_range)
+    prior0 = np.ones_like(r_t_range)/len(r_t_range)
+    prior0 /= prior0.sum()
+
+    # Create a DataFrame that will hold our posteriors for each day
+    # Insert our prior as the first posterior.
+    posteriors = pd.DataFrame(
+        index=r_t_range,
+        columns=sr.index,
+        data={sr.index[0]: prior0}
+    )
+
+    # We said we'd keep track of the sum of the log of the probability
+    # of the data for maximum likelihood calculation.
+    log_likelihood = 0.0
+
+    # (5) Iteratively apply Bayes' rule
+    for previous_day, current_day in zip(sr.index[:-1], sr.index[1:]):
+
+        #(5a) Calculate the new prior
+        current_prior = process_matrix @ posteriors[previous_day]
+
+        #(5b) Calculate the numerator of Bayes' Rule: P(k|R_t)P(R_t)
+        numerator = likelihoods[current_day] * current_prior
+
+        #(5c) Calcluate the denominator of Bayes' Rule P(k)
+        denominator = np.sum(numerator)
+
+        # Execute full Bayes' Rule
+        posteriors[current_day] = numerator/denominator
+
+        # Add to the running sum of log likelihoods
+        log_likelihood += np.log(denominator)
+
+    return posteriors, log_likelihood
